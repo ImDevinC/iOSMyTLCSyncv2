3289d4cec6eb981cd2f1fda94d172242e606247f
Devin Collins <devin@imdevinc.com>
2013-12-01 22:19:16 -0600

    Added GPL license information

--- MyTLC Sync/mytlcAddressViewController.h
+++ MyTLC Sync/mytlcAddressViewController.h
@@ -1,10 +1,21 @@
-//
-//  mytlcAddressViewController.h
-//  MyTLC Sync
-//
-//  Created by Devin Collins on 11/18/13.
-//  Copyright (c) 2013 Layer 8 Applications. All rights reserved.
-//
+/*
+ * Copyright 2013 Devin Collins <devin@imdevinc.com>
+ *
+ * This file is part of MyTLC Sync.
+ *
+ * MyTLC Sync is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MyTLC Sync is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MyTLC Sync.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #import <UIKit/UIKit.h>
 
